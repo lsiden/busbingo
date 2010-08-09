92dbf175e41688f0c43bd84d00844f68fc05aaf4
Larry Siden <lsiden@gmail.com>
2010-08-09 12:30:00 -0400

    Optimize recognition of bingo by checking only row and column of last tile covered, when available.

--- lib/bingoCard.rb
+++ lib/bingoCard.rb
@@ -5,22 +5,41 @@ class BingoCell < String
 end
 
 class BingoCard
+  # rawdata is in the form [[row0], ..., [row-n]].
+  # row-n is in the form [BingoCell-0, ..., BingoCell-m].
   def initialize(rawdata)
     @cells = rawdata.map {|row| row.map {|s| BingoCell.new(s)}}
     #pp @cells
   end
 
-  def bingo?
-    (0..4).each do |i|
-      return true if self.covered_row?(i)
+  # Returns true iff the card has a completely covered row, column, or diagonal.
+  # If row and col are non-nil, checks only that row and column, and diagonal 
+  # if row, col lies on either diagonal.
+  def bingo?(row=nil, col=nil)
+    if (row) then
+      return true if self.covered_row?(row)
+    else
+      (0..4).each do |i|
+        return true if self.covered_row?(i)
+      end
     end
 
-    (0..4).each do |i|
-      #pp self.column(i)
-      return true if self.covered_column?(i)
+    if (col) then
+      return true if self.covered_column?(col)
+    else
+      (0..4).each do |i|
+        #pp self.column(i)
+        return true if self.covered_column?(i)
+      end
     end
 
-    return risingDiagonal? || fallingDiagonal?
+    if (row.nil? || col.nil?) then
+      return covered_rising_diagonal? || covered_falling_diagonal?
+    elsif (row == col) then
+      return covered_falling_diagonal?
+    elsif (row + col == @cells.length - 1) then
+      return covered_rising_diagonal?
+    end
   end
 
   def covered_row?(i)
@@ -31,12 +50,12 @@ class BingoCard
     self.column(i).all? {|cell| cell.covered?}
   end
 
-  def risingDiagonal?
+  def covered_rising_diagonal?
     #pp self.risingDiagonal
     self.risingDiagonal.all? {|cell| cell.covered?}
   end
 
-  def fallingDiagonal?
+  def covered_falling_diagonal?
     self.fallingDiagonal.all? {|cell| cell.covered?}
   end
 
