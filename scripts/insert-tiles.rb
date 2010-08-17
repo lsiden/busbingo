b3d6eee3bf06e55e41477a544077230940b73acb
Larry Siden, Westside Consulting LLC <lsiden@westside-consulting.com>
2010-08-17 17:18:31 -0400

    Housecleaning

--- scripts/insert-tiles.rb
+++ scripts/insert-tiles.rb
@@ -1,19 +1,19 @@
-BusBingo::TileTemplate.create(:name => "1x", :desc => "Took bus 1 time in one week", :image_filename => "1X.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "3x", :desc => "Took bus 3 times in one week", :image_filename => "3X.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "5x", :desc => "Took bus 5 times in one week", :image_filename => "5X.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "bed", :desc => "Took a nap", :image_filename => "nap.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "bicyle", :desc => "Saw someone put a bike on the bus (including yourself)", :image_filename => "bike.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "books", :desc => "Read a book on the bus", :image_filename => "book.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "phone", :desc => "Made a phone call/sent a text message", :image_filename => "phone.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "cocktail", :desc => "Took bus to go to bar/restaurant", :image_filename => "drink.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "earphones", :desc => "Listened to music/podcast/radio", :image_filename => "Music.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "envelope", :desc => "Checked email", :image_filename => "email.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "home", :desc => "Took bus to go home", :image_filename => "home.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "bus-driver", :desc => "Said hi/Talked to the bus driver", :image_filename => "busdriver.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "shopping basket", :desc => "Took the bus to go shopping", :image_filename => "shopping.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "go!pass", :desc => "Saw someone use their go!pass", :image_filename => "gopass.jpg", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "conversation", :desc => "Had a conversation with another rider", :image_filename => "conversation.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "question", :desc => "Answered/asked a question on the bus", :image_filename => "question.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "walking", :desc => "Walked to a bus stop", :image_filename => "walked.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "work", :desc => "Took bus to go to work", :image_filename => "work.gif", :enabled => true).save
-BusBingo::TileTemplate.create(:name => "free!", :desc => "Free space", :image_filename => "free.png", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "1x", :desc => "Took bus 1 time in one week", :image_filename => "1X.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "3x", :desc => "Took bus 3 times in one week", :image_filename => "3X.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "5x", :desc => "Took bus 5 times in one week", :image_filename => "5X.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "bed", :desc => "Took a nap", :image_filename => "nap.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "bicyle", :desc => "Saw someone put a bike on the bus (including yourself)", :image_filename => "bike.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "books", :desc => "Read a book on the bus", :image_filename => "book.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "phone", :desc => "Made a phone call/sent a text message", :image_filename => "phone.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "cocktail", :desc => "Took bus to go to bar/restaurant", :image_filename => "drink.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "earphones", :desc => "Listened to music/podcast/radio", :image_filename => "Music.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "envelope", :desc => "Checked email", :image_filename => "email.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "home", :desc => "Took bus to go home", :image_filename => "home.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "bus-driver", :desc => "Said hi/Talked to the bus driver", :image_filename => "busdriver.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "shopping basket", :desc => "Took the bus to go shopping", :image_filename => "shopping.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "go!pass", :desc => "Saw someone use their go!pass", :image_filename => "gopass.jpg", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "conversation", :desc => "Had a conversation with another rider", :image_filename => "conversation.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "question", :desc => "Answered/asked a question on the bus", :image_filename => "question.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "walking", :desc => "Walked to a bus stop", :image_filename => "walked.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "work", :desc => "Took bus to go to work", :image_filename => "work.gif", :enabled => true).save
+BusBingo::TileTemplate.new(:name => "free!", :desc => "Free space", :image_filename => "free.png", :enabled => true).save
